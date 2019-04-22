09d94d2c0f4ce8ca06a2a8615b99f37e55878de4
wraithjpr <james.p.r.wraith@gmail.com>
2019-04-22 23:05:15 +0100

    Implemented applicative functions for Either: pure and apply

--- src/main/java/cloud/wraith/functional/Either.java
+++ src/main/java/cloud/wraith/functional/Either.java
@@ -9,13 +9,14 @@ import java.util.function.Supplier;
 /**
  * Either monad.
  *
- * <p>
- * {@code} data Either l a = Left l a | Right l a
+ * <p>{@code} data Either l a = Left l a | Right l a
  *
- * instance Functor Either where -- fmap :: (a -> b) -> Either l a -> Either l b
+ * <p>instance Functor Either where -- fmap :: (a -> b) -> Either l a -> Either l b
  * fmap _ (Left x _) = Left x _ fmap g (Right _ x) = Right _ (g x) {code}
  */
 public abstract class Either<L, A> {
+    @SuppressWarnings("unused")
+    private static final long serialVersionUID = 0L;
 
     /**
      * Hide the noargs constructor.
@@ -58,18 +59,16 @@ public abstract class Either<L, A> {
     /**
      * Functor fmap function.
      *
-     * <p>
-     * {@code} fmap :: (a -> b) -> Either l a -> Either l b {code}
+     * <p>{@code} fmap :: (a -> b) -> Either l a -> Either l b {code}
      *
-     * @param A   The right-hand type of the argument Either
-     * @param B   The right-hand type of the result Either
-     * @param L   The left-hand type of both argument and result Eithers
-     * @param fxn The mapping function; maps argument values in the type A to result
-     *            values in the type B
-     * @param fa  The source Either<L, A> functor
-     * @return The result Either<L, B> functor
+     * @param <A> The right-hand type of the argument Either
+     * @param <B> The right-hand type of the result Either
+     * @param <L> The left-hand type of both argument and result Eithers
+     * @param fxn The mapping function; maps argument values in the type A to result values in the type B
+     * @param fa  The source Either&lt;L,&nbsp;A&gt; functor
+     * @return The result Either&lt;L,&nbsp;B&gt; functor
      */
-    public static <A, B, L> Either<L, B> fmap(Function<? super A, ? extends B> fxn, Either<L, A> fa) {
+    public static <L, B, A> Either<L, B> fmap(Function<? super A, ? extends B> fxn, final Either<L, A> fa) {
         Objects.requireNonNull(fxn);
         Objects.requireNonNull(fa);
 
@@ -79,19 +78,64 @@ public abstract class Either<L, A> {
     /**
      * Map function.
      *
-     * @param A   The right-hand type of the argument Either
-     * @param B   The right-hand type of the result Either
-     * @param L   The left-hand type of both argument and result Eithers
-     * @param fxn The mapping function; maps argument values in the type A to result
-     *            values in the type B
-     * @return The result Either<L, B>
+     * @param <B> The right-hand type of the result Either
+     * @param fxn The mapping function; maps argument values in the type A to result values in the type B
+     * @return The result Either&lt;L,&nbsp;B&gt;
      */
     public abstract <B> Either<L, B> map(Function<? super A, ? extends B> fxn);
+
+    /**
+     * Applicative pure function.
+     * Converts a value of type A into a structure of type Either&lt;L,&nbsp;A&gt;.
+     * The result is right-handed.
+     *
+     * <p>{@code} pure :: a -> Right l a {code}
+     *
+     * @param <A>   The right-hand type of the result Either and the argument type
+     * @param <L>   The left-hand type of the result Either
+     * @param value The value of type A
+     * @return A structure of type Either&lt;L,&nbsp;A&gt;
+     */
+    public static <L, A> Either<L, A> pure(final A value) {
+        Objects.requireNonNull(value);
+
+        return right(value);
+    }
+
+    /**
+     * Applicative application function.
+     *
+     * @param <A> The right-hand type of the argument Either
+     * @param <B> The right-hand type of the result Either
+     * @param <L> The left-hand type of both argument and result Eithers
+     * @param ffxn The mapping function; maps argument values in the type A to result values in the type B
+     * @param fa  The source Either&lt;L,&nbsp;A&gt; functor
+     * @return The result Either&lt;L,&nbsp;B&gt; functor
+     */
+    public static <A, B, L> Either<L, B> fapply(Either<L, Function<? super A, ? extends B>> ffxn, final Either<L, A> fa) {
+        Objects.requireNonNull(ffxn);
+        Objects.requireNonNull(fa);
+
+        return ffxn.isLeft() ? Either.<L, B>left(ffxn.getLeft().get()) : fmap(ffxn.get().get(), fa);
+    }
+
+    /**
+     * Applicative application function.
+     *
+     * @param <A1> The right-hand type of the argument Either
+     * @param <B> The right-hand type of the result Either
+     * @param fa The functor to map using the mapping function wrapped by this Either
+     * @return The result Either&lt;L,&nbsp;B&gt;
+     */
+    public abstract <B, A1> Either<L, B> apply(Either<L, A1> fa);
 
     /**
      * Left implementation of Either.
      */
     static final class Left<L, A> extends Either<L, A> {
+        @SuppressWarnings("unused")
+        private static final long serialVersionUID = 0L;
+
         private final L value;
 
         private Left(final L value) {
@@ -165,23 +209,32 @@ public abstract class Either<L, A> {
         }
 
         @Override
+        public <B, A1> Either<L, B> apply(Either<L, A1> fa) {
+            return Either.<L, B>left(value);
+        }
+
+        @Override
         public String toString() {
             return String.format("Either.left[%s]", value.toString());
         }
 
         @Override
         public boolean equals(final Object obj) {
-            if (this == obj)
+            if (this == obj) {
                 return true;
-            if (obj == null)
+            }
+            if (obj == null) {
                 return false;
-            if (!(obj instanceof Either))
+            }
+            if (!(obj instanceof Either)) {
                 return false;
+            }
 
             Either<?, ?> other = (Either<?, ?>) obj;
 
-            if (!other.isLeft())
+            if (!other.isLeft()) {
                 return false;
+            }
 
             return Objects.equals(value, other.getLeft().get());
         }
@@ -189,9 +242,12 @@ public abstract class Either<L, A> {
     }
 
     /**
-     * Right implementation of Either
+     * Right implementation of Either.
      */
     static final class Right<L, A> extends Either<L, A> {
+        @SuppressWarnings("unused")
+        private static final long serialVersionUID = 0L;
+
         private final A value;
 
         private Right(final A value) {
@@ -267,23 +323,38 @@ public abstract class Either<L, A> {
         }
 
         @Override
+        public <B, A1> Either<L, B> apply(Either<L, A1> fa) {
+            Objects.requireNonNull(fa);
+            Objects.requireNonNull(value instanceof Function<?, ?> ? value : null);
+
+            @SuppressWarnings("unchecked")
+            Function<? super A1, ? extends B> fxn = (Function<? super A1, ? extends B>) value;
+
+            return fa.map(fxn);
+        }
+
+        @Override
         public String toString() {
             return String.format("Either.right[%s]", value.toString());
         }
 
         @Override
         public boolean equals(final Object obj) {
-            if (this == obj)
+            if (this == obj) {
                 return true;
-            if (obj == null)
+            }
+            if (obj == null) {
                 return false;
-            if (!(obj instanceof Either))
+            }
+            if (!(obj instanceof Either)) {
                 return false;
+            }
 
             Either<?, ?> other = (Either<?, ?>) obj;
 
-            if (!other.isRight())
+            if (!other.isRight()) {
                 return false;
+            }
 
             return Objects.equals(value, other.get().get());
         }
